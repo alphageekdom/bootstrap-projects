e33bb7c6ee0494cc272583d4d39a3f8b2d1074ed
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-09-19 15:57:28 -0700

    footer

--- vera-software-solutions/js/script.js
+++ vera-software-solutions/js/script.js
@@ -1,13 +1,17 @@
-function userScroll() {
+// Smooth Scrolling
+
+const userScroll = () => {
   const navbar = document.querySelector('.navbar');
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
       navbar.classList.add('bg-dark');
+      navbar.classList.add('navbar-sticky');
     } else {
       navbar.classList.remove('bg-dark');
+      navbar.classList.remove('navbar-sticky');
     }
   });
-}
+};
 
 document.addEventListener('DOMContentLoaded', userScroll);

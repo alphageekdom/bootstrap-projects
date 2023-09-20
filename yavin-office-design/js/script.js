2b781ace215f7228e084be7a4faef6603cba3c68
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-09-20 10:07:07 -0700

    navbar smooth scrolling

---
+++ yavin-office-design/js/script.js
@@ -0,0 +1,14 @@
+// Smooth Scroll
+const userScroll = () => {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', userScroll);

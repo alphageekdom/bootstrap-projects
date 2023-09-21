6878d96a58b0a379b668f1812af7bd62fc7c16e2
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-09-20 18:51:52 -0700

    navbar functionality

--- vera-software-solutions/js/script.js
+++ vera-software-solutions/js/script.js
@@ -10,3 +10,26 @@ if (checkReplace !== null) {
     autoRun: true,
   });
 }
+
+// Smooth Scroll
+const userScroll = () => {
+  const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('#to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+    }
+  });
+};
+
+// Even Listeners
+document.addEventListener('DOMContentLoaded', userScroll);
